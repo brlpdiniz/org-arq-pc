cef2a74516dc27920eaf1adb23560e7813259b95
Bruno Diniz <brunoldiniz14@gmail.com>
2023-10-26 22:16:30 -0300

    feat: ED - Assembly

---
+++ ED/2Bimestre/Assembly/somaNumeros.asm
@@ -0,0 +1,11 @@
+; MEU PRIMEIRO CÓDIGO
+; Somar 2 + 2
+
+START:
+; Salva o 2 no registrador A:
+	MOV A, 10
+	MOV B, 2
+	ADD A, B
+
+	MOV C, 5
+	ADD A, C
---
+++ ExercicioSala/2023_10_26.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+void main() {
+    int result = 0;
+
+    result = 3 + 2;
+
+    printf("%d", result);
+
+    return;
+}
